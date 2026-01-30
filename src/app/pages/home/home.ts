dfaa95f3132f4ee5f8d4b18261f3539456c50ef2
Rucel Tsafack <tsafackrucel@gmail.com>
2026-01-31 00:33:41 +0100

    update home page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
-import { FormDisease } from "../../components/form-disease/form-disease";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: 'app-home',
-  imports: [MatInputModule, MatButtonModule, FormDisease],
+  imports: [MatInputModule, MatButtonModule, MatIconModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,33 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#135bec",
+        accent: {
+          emerald: "#10b981"
+        },
+        background: {
+          light: "#f6f6f8",
+          dark: "#0a0a0b"
+        }
+      },
+      fontFamily: {
+        display: ["Manrope", "sans-serif"]
+      },
+      borderRadius: {
+        DEFAULT: "0.5rem",
+        lg: "1rem",
+        xl: "1.5rem",
+        full: "9999px"
+      }
+    }
   },
   plugins: [
-    require("daisyui")
-  ],
+    
+  ]
 }
